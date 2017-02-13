fba1aa3767f2fb10ec2859f4f2a8ca7ba5793a15
ruselll1705 <ruselll1705@gmail.com>
2017-02-13 21:24:27 +0200

    lection_3(class worck)

---
+++ lection_3.py
@@ -0,0 +1,13 @@
+#import sys,math
+
+#x=float(sys.argv[1])
+#y=math.sqrt(x)
+#print(y)
+
+#from math import sin, cos, pi
+#x=int(input())
+#y=sin(x)**2+cos(x)**2
+#print(y)
+#from math import *
+
+
---
+++ lection_3_1.py
@@ -0,0 +1,32 @@
+# s=input()
+# if s=='Hi':
+#     pass
+#     print('Hellow')
+# print('Hellow')
+
+# x=int(input())
+# if 0<x<=100:
+#     print('Correct number')
+# else:
+#     print('Incorrect number')
+
+# x=int(input())
+# if 0<x<=100:
+#     print('Correct number')
+# elif x<0:
+#       print("I don't want a negativ number")
+# else:
+#     print("number > 100: eror")
+
+# x=input()
+# if x.isdigit()==True:
+#     d=int(x)*4
+#     print(d)
+# else:
+#     print('eror')
+while True:
+    print('vecnii cikl')
+
+
+
+
