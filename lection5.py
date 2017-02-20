169ddc6f7803f82e7d2050e6ae59a3d2cb218184
ruselll1705@gmail.com <ruselll1705@gmail.com>
2017-02-20 21:48:23 +0200

    lection5

---
+++ lection5.py
@@ -0,0 +1,39 @@
+# f = open ("test4.txt")
+# for i in f:
+#     print(i.rstrip()+"!")
+# print(type(f))
+# s='world'
+# for i in s:
+#     print(i*3, end='')
+s=[1,2,4,5,6,7,8]
+# s[2]=[1,3,6]
+# print(s)
+# s[2][-1]='yeloww'
+# print(s)
+# s[4:4]=['r','d','g']
+# print(s)
+# s *=3
+# print(s)
+# print('span' in s)
+# print('eggs' not in s)
+# l=[1,3,5,6,7,9]
+# l.append([1,4,6])
+# print(l)
+# print(l.index([1,4,6]))
+# l[l.index([1,4,6])].append([4,5])
+# print(l)
+# l.insert(len(l)//2, 'dir')
+# print(l)
+# l.extend(s)
+# print(l)
+# print(s+s)
+# l.remove(5)
+# print(l)
+# p=[x **3 for x in range(1,25,2)]
+# print(p)
+# i=[1,2,5,7]
+# d=[r**2 for r in i]
+# print(d)
+k=['er','gr','gd','ser']
+d=[r[0] for r in k]
+print(d)
