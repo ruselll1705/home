5d9bd37894a04257405102059f38ff740aa166b1
ruselll1705@gmail.com <ruselll1705@gmail.com>
2017-03-13 21:37:14 +0200

    лекция10

---
+++ lection10.py
@@ -0,0 +1,9 @@
+# s = "{srt} {count}"
+# s = s.format (srt='one', count='two')
+# print(s)
+# dict = {"nam": "rusel"}
+# s = "person,{nam} "
+# s = s.format(nam=dict)
+# print(s)
+for i in range(1000):
+    print(str("{:c}".format(i)))
